f309d7c7837cc2ba6e160a943906dbd548ed56c9
Nathaniel Nunes <manutdkid77@gmail.com>
2019-06-05 12:10:14 +0530

    initial commit

---
+++ FlexiGallery/FlexiGallery/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using FlexiGallery.Models;
+
+namespace FlexiGallery.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
