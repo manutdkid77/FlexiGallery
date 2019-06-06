862db8ea17c593037633e7653e9a4bfebe6fdd2e
Nathaniel Nunes <manutdkid77@gmail.com>
2019-06-06 17:19:53 +0530

    added GalleryPage which holds a flexlayout with basis of 0.33%

---
+++ FlexiGallery/FlexiGallery/Views/GalleryPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XE = Xamarin.Essentials;
+using Debug = System.Diagnostics.Debug;
+
+namespace FlexiGallery.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GalleryPage : ContentPage
+    {
+        public GalleryPage()
+        {
+            InitializeComponent();
+            this.SizeChanged += GalleryPage_SizeChanged;
+        }
+
+        private void GalleryPage_SizeChanged(object sender, EventArgs e)
+        {
+            //var dbDeviceWidth = XE.DeviceDisplay.MainDisplayInfo.Width;
+            //Debug.WriteLine($"{nameof(dbDeviceWidth)} is {dbDeviceWidth}");
+            //var fltFlexBasis = (float)((dbDeviceWidth / 3) / 100);
+            //var fltFlexBasis = (float)(dbDeviceWidthSplit / 3) / 100;
+
+            var fltFlexBasis = (((float)100 / 3) / 100);
+
+            foreach (var child in flxContainer.Children)
+            {
+                FlexLayout.SetBasis(child, new FlexBasis(fltFlexBasis, isRelative:true));
+            }
+        }
+    }
+}
